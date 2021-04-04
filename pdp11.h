85e13b41c93816a0ee1b789e15b6a5a82e6b06db
Yurchenko Veronika <iurchenko.v@phystech.edu>
2021-04-04 19:42:13 +0300

    pdp11.h

--- pdp11.h
+++ pdp11.h
@@ -5,11 +5,11 @@ typedef word Adress;
 #define MEM_SIZE (64*1024)
 #define pc reg[7]
 #define sp reg[6]
-#define odata 0177566
-#define ostat 0177564
+#define ODATA 0177566
+#define OSTAT 0177564
 
 void trace(const char * format, ...);
-void test();
+void test_mem();
 void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
