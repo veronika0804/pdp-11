8dd67734a0908c6cb6106a9097bafe1d5728961d
Yurchenko Veronika <iurchenko.v@phystech.edu>
2021-04-18 15:13:56 +0300

    sum

---
+++ cmake-build-debug/CMakeFiles/pdp11.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/pdp11/accert.c" "D:/pdp11/cmake-build-debug/CMakeFiles/pdp11.dir/accert.c.obj"
+  "D:/pdp11/pdp_mem.c" "D:/pdp11/cmake-build-debug/CMakeFiles/pdp11.dir/pdp_mem.c.obj"
+  "D:/pdp11/pdp_run.c" "D:/pdp11/cmake-build-debug/CMakeFiles/pdp11.dir/pdp_run.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/pdp11.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdp11.dir/accert.c.obj"
+  "CMakeFiles/pdp11.dir/pdp_mem.c.obj"
+  "CMakeFiles/pdp11.dir/pdp_run.c.obj"
+  "pdp11.exe"
+  "pdp11.exe.manifest"
+  "pdp11.lib"
+  "pdp11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pdp11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
