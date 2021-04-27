4044928b8d0b9f8456ccdb33ceb908f69d3e45f3
Yurchenko Veronika <iurchenko.v@phystech.edu>
2021-04-27 17:14:21 +0300

    sob, br, run

--- pdp_run.c
+++ pdp_run.c
@@ -3,13 +3,20 @@
 #include "pdp11.h"
 
 word reg[8];
-byte b;
+byte b, r;
 Arg ss, dd;
+word nn;
+char xx;
+byte N, Z, C;
 
 void print_regs() {
     for (int i= 0; i < 8; i++)
         trace("R%o = %06o\n", i, reg[i]);
     trace("\n");
+}
+
+void print_flags() {
+    trace("N = %d\n Z= %d\n C= %d\n", N, Z, C);
 }
 
 void do_halt() {
@@ -28,6 +35,10 @@ void do_mov(word w) {
             w_write(dd.adr, ss.val);
         else
             b_write(dd.adr, (byte)(ss.val & 0xFF));
+    C = 0;
+    Z = (ss.val == 0);
+    N = (ss.val >> 15) & 1;
+    trace("\n");
 }
 void do_add(word w) {
     b = (w >> 15) & 1;
@@ -40,7 +51,28 @@ void do_add(word w) {
             w_write(dd.adr, dd.val + ss.val);
         else
             b_write(dd.adr, dd.val + ss.val);
+
+    Z = (dd.val + ss.val ==0);
+    N = ((dd.val + ss.val) >> 15) & 1;
+    C = ((dd.val + ss.val) >= MEM_SIZE);
+    trace("\n");
 }
+
+void sob(word w) {
+    r = (w >> 6) & 7;
+    nn = w & 63;
+    if (--reg[r] != 0)
+        pc = pc - 2 * nn;
+    trace("R%o %06o", r, pc);
+    trace("\n");
+}
+
+void br(word w) {
+    xx = (char)(w & 0377);
+    pc = pc + 2 * xx;
+    trace("%06o\n", pc);
+}
+
 void do_nothing(word w) {
     trace("\n");
 }
@@ -49,13 +81,15 @@ typedef struct {
     word mask;
     word opcode;
     char * name;
-    void (*do_func)(void);
+    void (*do_func)(word w);
 } Command;
 
 Command cmd[] = {
         {0170000, 0010000, "mov", do_mov},
         {0170000, 0060000, "add", do_add},
         {0177777, 0000000, "halt", do_halt},
+        {0177000, 0077000, "sob", sob},
+        {0177400, 0000400, "br", br},
         {0177777, 0177777, "nothing", do_nothing}
 };
 
@@ -116,15 +150,23 @@ void run() {
         word w = w_read(pc);
         trace("%06o %06o: ", pc, w);
         pc += 2;
-        if (w == 0) {
-            trace("halt ");
-            do_halt();
+        int i = 0;
+        while(1) {
+            if ((w & cmd[i].mask) == cmd[i].opcode) {
+                trace("%s ", cmd[i].name);
+                cmd[i].do_func(w);
+                break;
+            }
+            i++;
+            if ((cmd[i].mask == 0177777) && (cmd[i].opcode == 0177777) ) {
+                trace("%s ", cmd[i].name);
+                cmd[i].do_func(w);
+                break;
+            }
         }
-        else if ((w & 0170000) == 0010000) { //01SSDD
-            trace("mov ");
-            ss = get_mr(w >> 6);
-            dd = get_mr(w);
-            do_mov();
+        if (trac == 2) {
+            print_regs();
+            print_flags();
         }
     }
 }
