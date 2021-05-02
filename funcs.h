b3bc689f6c97d7c3358e22233c5995f160440c96
Yurchenko Veronika <iurchenko.v@phystech.edu>
2021-05-02 19:16:37 +0300

    +funcs

--- funcs.h
+++ funcs.h
@@ -5,3 +5,8 @@ void do_add(word w);
 void do_sob(word w);
 void do_br(word w);
 void do_nothing(word w);
+void do_cmp(word w);
+void do_beq(word w);
+void do_bpl(word w);
+void do_clr(word w);
+void do_tst(word w);
