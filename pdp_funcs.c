f2434ac7b84bd0736283ca69a9d617e62a958880
Yurchenko Veronika <iurchenko.v@phystech.edu>
2021-05-02 19:14:45 +0300

    +funcs

--- pdp_funcs.c
+++ pdp_funcs.c
@@ -17,10 +17,10 @@ void do_mov(word w) {
     if (dd.type == 0)
         reg[dd.adr] = ss.val;
     else
-    if (b == 0)
-        w_write(dd.adr, ss.val);
-    else
-        b_write(dd.adr, (byte)(ss.val & 0xFF));
+        if (b == 0)
+            w_write(dd.adr, ss.val);
+        else
+            b_write(dd.adr, (byte)(ss.val & 0xFF));
     C = 0;
     Z = (ss.val == 0);
     N = (ss.val >> 15) & 1;
@@ -57,6 +57,76 @@ void do_br(word w) {
     xx = (char)(w & 0377);
     pc = pc + 2 * xx;
     trace("%06o\n", pc);
+}
+
+void do_bpl(word w) {
+    if (N == 0)
+        do_br(w);
+    else
+        trace("FALSE\n");
+}
+
+void do_beq(word w) {
+    if (Z == 1)
+        do_br(w);
+    else
+        trace("FALSE\n");
+}
+
+void do_cmp(word w) {
+    b = (w >> 15) & 1;
+    ss = get_mr(w >> 6);
+    dd = get_mr(w);
+    Z = ((ss.val - dd.val) == 0);
+    if (b == 0) {
+        N = ((ss.val - dd.val) >> 15) & 1;
+        C = ((ss.val + 1 + ~dd.val) >= MEM_SIZE);
+    }
+    else {
+        N = (((byte)ss.val - (byte)dd.val) >> 7) & 1;
+        C = (((byte)ss.val + 1 + ~((byte)dd.val)) >= 256);
+    }
+    trace("\n");
+}
+
+void do_clr(word w) {
+    b = (w >> 15) & 1;
+    dd = get_mr(w);
+    if (dd.type = 0)
+        reg[dd.adr] = 0;
+    else
+    if (b == 0)
+        w_write(dd.adr, 0);
+    else
+        b_write(dd.adr, 0);
+
+    Z = 1;
+    N = 0;
+    C = 0;
+    trace("\n");
+}
+
+void do_tst(word w) {
+    C = 0;
+    b = (w >> 15) & 1;
+    dd = get_mr(w);
+    if (dd.type == 0) {
+        Z = (reg[dd.adr] == 0);
+        if (b == 0)
+            N = ((reg[dd.adr] >> 15) & 1);
+        else
+            N = ((reg[dd.adr] >> 7) & 1);
+    } else {
+        if (b == 0) {
+            Z = (w_read(dd.adr) == 0);
+            N = ((w_read(dd.adr) >> 15) & 1);
+        } else {
+            Z = (b_read(dd.adr) == 0);
+            N = ((b_read(dd.adr) >> 7) & 1);
+        }
+    }
+    C = 0;
+    trace("\n");
 }
 
 void do_nothing(word w) {
