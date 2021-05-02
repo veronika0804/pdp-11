6e4d0812b238e377505be907702218fc04e2b64f
Yurchenko Veronika <iurchenko.v@phystech.edu>
2021-05-02 19:19:36 +0300

    tests full

--- pdp_mem.c
+++ pdp_mem.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 #include "pdp11.h"
 #include "run.h"
 
@@ -21,37 +22,37 @@ void trace(const char* format, ...) {
 
 void test_mem() {
     //пишем байт, читаем байт
-    byte b0 = 0x0a;
-    b_write(2, b0);
-    byte bres1 = b_read(2);
-    printf("%02hhx = %02hhx\n", b0, bres1);
-    assert(b0 == bres1);
+    byte b1 = 0x0a;
+    b_write(0, b1);
+    byte bres = b_read(0);
+    trace("%02hhx = %02hhx\n", b1, bres);
+    assert(bres == b1);
 
-    //пишем слово читаем слово
+    //пишем слово, читаем слово
     word w1 = 0x0a0b;
     w_write(0, w1);
-    word wres1 = w_read(0);
-    assert(wres1 == w1);
+    word wres = w_read(0);
+    trace("%04hx = %04hx\n", w1, wres);
+    assert(wres == w1);
 
     //пишем 2 байта, читаем слово
-    byte b1 = 0x0b;
-    b0 = 0x0a;
-    word w = 0x0b0a;
-    b_write(0, b0);
-    b_write(1, b1);
-    word wres2 = w_read(0);
-    printf("ww/br \t %04hx=%02hhx%02hhx\n", b1, b0, wres2);
-    assert(wres2 == w);
+    b1 = 0x0a;
+    byte b2 = 0x0b;
+    b_write(0, b1);
+    b_write(1, b2);
+    wres = w_read(0);
+    trace("%02hhx%02hhx = %04hx\n", b2, b1, wres);
+    assert(wres == 0x0b0a);
 
     //пишем слово, читаем 2 байта
-    word w0 = 0x0a0b;
-    w_write(2, w0);
-    b0 = b_read(2);
-    b1 = b_read(3);
-    printf("%04hx\n%02hhx%02hhx\n", w0, b1, b0);
-    assert(b0 == (byte)0x0b);
-    assert(b1 == (byte)0x0a);
-
+    w1 = 0x0b0a;
+    w_write(0, w1);
+    byte bres1 = b_read(0);
+    byte bres2 = b_read(1);
+    trace("%02hhx = 0a\n", bres1);
+    trace("%02hhx = 0b\n", bres2);
+    assert(bres1 == 0x0a);
+    assert(bres2 == 0x0b);
 }
 
 word w_read(Adress a) {
@@ -81,6 +82,11 @@ void load_file(const char* filename) {
     byte b, c;
     FILE* input_file;
     input_file = fopen(filename, "r");
+
+    if (input_file == NULL) {
+        perror(filename);
+        exit(1);
+    }
     while (fscanf(input_file, "%hx %hx", &a, &n) == 2) {
         for (Adress i = 0; i < n; i++) {
             fscanf(input_file, "%hhx", &c);
